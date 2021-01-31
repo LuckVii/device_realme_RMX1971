2824ecbea1bae119a7072854f81bc6d98c5a9779
lucky <ah440099@gmail.com>
2021-01-31 15:21:29 +0200

    RMX1971: BringUp awakenOS

--- awaken_RMX1971.mk
+++ awaken_RMX1971.mk
@@ -4,8 +4,8 @@
 # SPDX-License-Identifer: Apache-2.0
 #
 
-# Inherit some common lineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common.mk)
+# Inherit some common awakenOS stuff.
+$(call inherit-product, vendor/awaken/config/common.mk)
 
 # Inherit from RMX1971 device.
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -15,7 +15,7 @@ TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_DEVICE := RMX1971
-PRODUCT_NAME := lineage_RMX1971
+PRODUCT_NAME := awaken_RMX1971
 PRODUCT_BRAND := Realme
 PRODUCT_MODEL := Realme 5 Pro
 PRODUCT_MANUFACTURER := Realme
